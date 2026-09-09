3e15cf80ee63d59ac816e7c41772e0f7da71d73c
TkachukBogdan <bogdantkacuk77@gmail.com>
2026-09-09 09:44:44 +0300

    added main page

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title>Оренда обладнання</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Сайт замовлення обладнання для подій</h1>
+<p><a href="../views/form.php">Зробити нове замовлення</a></p>
+<p><a href="../views/history.php">Переглянути історію замовлень</a></p>
+
+</body>
+</html>
